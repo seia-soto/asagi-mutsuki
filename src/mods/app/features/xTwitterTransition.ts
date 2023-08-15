bed914905a5c3e9600946a160b511916747264cb
HoJeong Go <seia@outlook.kr>
2023-08-15 10:11:29 +0900

    feat: suppress embeds on original message

--- src/mods/app/features/xTwitterTransition.ts
+++ src/mods/app/features/xTwitterTransition.ts
@@ -2,6 +2,9 @@ import {type Client, type Message, type PossiblyUncachedTextableChannel} from 'e
 
 import {downstreamEvents, uncontrollableChannels} from '../downstream.js';
 
+// eslint-disable-next-line no-bitwise
+const suppressEmbeds = 1 << 2;
+
 const handleMessageCreate = async (client: Client, message: Message<PossiblyUncachedTextableChannel>) => {
 	const isChannelControllable = uncontrollableChannels.consume(message.channel.id);
 
@@ -16,17 +19,26 @@ const handleMessageCreate = async (client: Client, message: Message<PossiblyUnca
 		return;
 	}
 
-	const response = await client.createMessage(message.channel.id, {
-		allowedMentions: {
-			repliedUser: false,
-		},
-		content: links.map(link => 'https://' + link.toString().replace('x.com', 'twitter.com')).join('\n'),
-		messageReference: {
-			// eslint-disable-next-line @typescript-eslint/naming-convention
-			messageID: message.id,
-		},
-	})
-		.then(() => undefined)
+	const response = await (async () => {
+		await Promise.all([
+			client.createMessage(message.channel.id, {
+				allowedMentions: {
+					repliedUser: false,
+				},
+				content: links.map(link => 'https://' + link.toString().replace('x.com', 'twitter.com')).join('\n'),
+				messageReference: {
+					// eslint-disable-next-line @typescript-eslint/naming-convention
+					messageID: message.id,
+				},
+			}),
+			client.editMessage(message.channel.id, message.id, {
+				// eslint-disable-next-line no-bitwise
+				flags: suppressEmbeds | message.flags,
+			}),
+		]);
+
+		return true;
+	})()
 		.catch((error: Error) => {
 			if (!error.name.toLocaleLowerCase().includes('permission')) {
 				console.error(`Unexpected channel handling of message='${message.id}' channel='${message.channel.id}'`);
