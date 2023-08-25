b656f0babd916cd990ef8cd7c53aa64bc19ce97d
HoJeong Go <seia@outlook.kr>
2023-08-26 07:59:12 +0900

    feat: support referencing emoji creation in emoji magnifier

--- src/integrations/discord/bootstrap.ts
+++ src/integrations/discord/bootstrap.ts
@@ -1,7 +1,8 @@
-import {type ApplicationCommand, Client} from 'eris';
+import {type ApplicationCommand, Client, type Emoji} from 'eris';
 
 import {type Mutsuki} from '../../index.js';
 import {aDiscordToken, useEnv} from '../../mods/env.js';
+import {TimescaleMap} from '../../mods/hashmap.js';
 import {BucketLimiter, RateLimiter} from '../../mods/ratelimit.js';
 import {enableBskyLoader} from './features/bskyLoader.js';
 import {enableDeleteMy} from './features/deleteMy.js';
@@ -10,6 +11,9 @@ import {enableXtwitterTransition} from './features/xTwitterTransition.js';
 import {DownstreamEventEmitter} from './mods/downstream.js';
 
 export type MutsukiDiscordIntegration = {
+	caches: {
+		guildEmotes: TimescaleMap<Emoji[]>;
+	};
 	client: Client;
 	commands: ApplicationCommand[];
 	downstream: DownstreamEventEmitter;
@@ -26,12 +30,16 @@ export type MutsukiDiscordIntegration = {
 };
 
 export const aMutsukiDiscordIntegration: () => MutsukiDiscordIntegration = () => ({
+	caches: {
+		guildEmotes: new TimescaleMap(),
+	},
 	client: new Client(aDiscordToken, {
 		intents: [
 			'guildEmojis',
 			'guildMembers',
 			'guildMessages',
 		],
+		restMode: true,
 	}),
 	commands: [],
 	downstream: new DownstreamEventEmitter(),
--- src/integrations/discord/features/emojiMagnifier.ts
+++ src/integrations/discord/features/emojiMagnifier.ts
@@ -1,4 +1,6 @@
-import {type Message, type MessageContent, type PossiblyUncachedTextableChannel} from 'eris';
+import {type ApplicationCommandStructure, type CommandInteraction, Constants, type Emoji, type Guild, type Message, type MessageContent, type PossiblyUncachedTextableChannel} from 'eris';
+import got from 'got';
+import sharp from 'sharp';
 
 import {type Mutsuki} from '../../../index.js';
 import {aControlChannelContext} from '../mods/controlChannel.js';
@@ -7,8 +9,126 @@ const getAnimatedEmojiUrl = (id: string) => `https://cdn.discordapp.com/emojis/$
 
 const getStaticEmojiUrl = (id: string) => `https://cdn.discordapp.com/emojis/${id}.png?size=256`;
 
+const commandName = 'snapshot_emote';
+
+const optionName = 'animated_emote';
+
+const applicationCommand: ApplicationCommandStructure = {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	default_member_permissions: true,
+	description: 'Snapshot an animated emote and make a static emote linked with',
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	description_localizations: {
+		ko: '움직이는 이모지를 캡쳐 후 연동되는 정적 이모지 생성',
+	},
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	dm_permission: false,
+	name: commandName,
+	options: [
+		{
+			description: 'An animated emote',
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			description_localizations: {
+				ko: '움직이는 이모지',
+			},
+			name: optionName,
+			required: true,
+			type: Constants.ApplicationCommandOptionTypes.STRING,
+		},
+	],
+	type: Constants.ApplicationCommandTypes.CHAT_INPUT,
+};
+
+const handleInteractionCreate = async (mutsuki: Mutsuki, interaction: CommandInteraction) => {
+	if (
+		interaction.type !== Constants.InteractionTypes.APPLICATION_COMMAND
+		|| !interaction.guildID
+		|| !interaction.member
+		|| interaction.data.name !== commandName
+		|| interaction.data.options?.[0].name !== optionName
+		|| interaction.data.options[0].type !== Constants.ApplicationCommandOptionTypes.STRING
+	) {
+		return;
+	}
+
+	const animatedEmoteName = interaction.data.options[0].value;
+
+	if (!/^\w+$/.test(animatedEmoteName)) {
+		await interaction.createMessage('❎');
+
+		return;
+	}
+
+	const {discord} = mutsuki.integrations;
+
+	const cachedEmote = discord.caches.guildEmotes.pull(interaction.guildID);
+	let emotes: Emoji[];
+
+	if (cachedEmote) {
+		emotes = cachedEmote.value;
+	} else {
+		const live = await discord.client.getRESTGuildEmojis(interaction.guildID);
+
+		discord.caches.guildEmotes.push(interaction.guildID, live);
+
+		emotes = live;
+	}
+
+	const emote = emotes.find(emote => emote.animated && emote.name === animatedEmoteName);
+
+	if (!emote) {
+		await interaction.createMessage('🧐');
+
+		return;
+	}
+
+	const referencingEmoteName = animatedEmoteName + 'xff' + emote.id;
+
+	if (emotes.find(emote => emote.name === referencingEmoteName)) {
+		await interaction.createMessage('😓');
+
+		return;
+	}
+
+	const response = await got(getStaticEmojiUrl(emote.id), {
+		headers: {
+			'user-agent': 'seia-soto/asagi-mutsuki',
+		},
+	})
+		.buffer();
+
+	const image = await sharp(response, {pages: 1})
+		.resize({height: 128, width: 128})
+		.webp({effort: 2})
+		.toBuffer();
+
+	await discord.client.createGuildEmoji(interaction.guildID, {
+		image: 'data:image/webp;base64,' + image.toString('base64'),
+		name: referencingEmoteName,
+	});
+	await interaction.createMessage('✅');
+};
+
+const handleGuildEmojisUpdate = async (mutsuki: Mutsuki, guild: Guild, emojis: Emoji[], removedEmojis: Emoji[] | undefined) => {
+	const {discord} = mutsuki.integrations;
+
+	const cachedEmote = discord.caches.guildEmotes.pull(guild.id);
+
+	if (!cachedEmote) {
+		return;
+	}
+
+	let updatedEmotes = [...cachedEmote.value, ...emojis];
+
+	if (removedEmojis) {
+		updatedEmotes = cachedEmote.value.filter(emote => !removedEmojis.find(removedEmoji => emote.id === removedEmoji.id));
+	}
+
+	discord.caches.guildEmotes.push(guild.id, updatedEmotes);
+};
+
 const handleMessageCreate = async (mutsuki: Mutsuki, message: Message<PossiblyUncachedTextableChannel>) => aControlChannelContext(mutsuki, message.channel.id, async aContext => {
-	const singleEmojiPattern = /^<a?:[a-zA-Z\d_]+:(\d+)>$/i;
+	const singleEmojiPattern = /^<a?:([a-zA-Z\d_]+):(\d+)>$/i;
 	const emojiIdMatcher = singleEmojiPattern.exec(message.content);
 
 	if (!emojiIdMatcher) {
@@ -19,8 +139,11 @@ const handleMessageCreate = async (mutsuki: Mutsuki, message: Message<PossiblyUn
 
 	const {discord} = mutsuki.integrations;
 
-	const [, emojiId] = emojiIdMatcher;
-	const isAnimatedEmoji = message.content.startsWith('<a:');
+	const [, emoteName, originalEmoteId] = emojiIdMatcher;
+	const isForwardedEmote = /xff\d+$/.test(emoteName);
+	const emoteId = isForwardedEmote ? emoteName.split('xff')[1] : originalEmoteId;
+
+	const isAnimatedEmoji = isForwardedEmote || message.content.startsWith('<a:');
 
 	const copy: MessageContent = {
 		embed: {
@@ -31,7 +154,7 @@ const handleMessageCreate = async (mutsuki: Mutsuki, message: Message<PossiblyUn
 			},
 			color: message.author.accentColor ?? 0,
 			image: {
-				url: isAnimatedEmoji ? getAnimatedEmojiUrl(emojiId) : getStaticEmojiUrl(emojiId),
+				url: isAnimatedEmoji ? getAnimatedEmojiUrl(emoteId) : getStaticEmojiUrl(emoteId),
 			},
 		},
 	};
@@ -55,7 +178,19 @@ const handleMessageCreate = async (mutsuki: Mutsuki, message: Message<PossiblyUn
 export const enableEmojiMagnifier = async (mutsuki: Mutsuki) => {
 	const {discord} = mutsuki.integrations;
 
+	const command = discord.commands.find(command => command.name === commandName);
+
+	if (command) {
+		if (mutsuki.integrations.discord.options.shouldUpdateApplicationCommands) {
+			await discord.client.editCommand(command.id, applicationCommand);
+		}
+	} else {
+		await discord.client.createCommand(applicationCommand);
+	}
+
 	discord.downstream.on('filteredMessageCreate', handleMessageCreate);
+	discord.client.on('guildEmojisUpdate', handleGuildEmojisUpdate);
+	discord.client.on('interactionCreate', handleInteractionCreate.bind(null, mutsuki));
 
 	mutsuki.logger.info('enabled emoji magnifier');
 };
